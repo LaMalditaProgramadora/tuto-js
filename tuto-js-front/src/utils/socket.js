3ea610f8c16bfccf887ad3501b9e8c2697232f77
LaMalditaProgramadora <jyellow363@gmail.com>
2022-05-22 23:17:04 -0500

    .

--- tuto-js-front/src/utils/socket.js
+++ tuto-js-front/src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const url = `${process.env.REACT_APP_TUTO_HEROKU_EXT}`;
+const url = `${process.env.REACT_APP_TUTO_HEROKU}`;
 console.log(url);
 
 export default io(url);
